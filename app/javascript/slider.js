d99ff1b16988eb7c4e0487cee2a078b06805a9b0
laketych1327 <laketych@gmail.com>
2020-03-22 19:05:03 +0200

    desktop version

---
+++ app/javascript/slider.js
@@ -0,0 +1,10 @@
+const mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
